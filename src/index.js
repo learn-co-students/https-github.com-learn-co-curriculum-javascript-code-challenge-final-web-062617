39c1a5af86fa98bd0a6de7f2fbcb0cc7c6e99f69
Jacob Harrison Lyon <jacobhlyon@gmail.com>
2017-08-25 12:00:49 -0400

    finished project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
-  // your code here!
+  
+	$("#new-image").modal()
+
+	let allImages = []
+
+	const imageUrl = document.getElementById("image-url") 
+	const imageCaption = document.getElementById("caption")
+	const submitImage = document.getElementById("new-image-submit-button")
+	const imagesContainer = document.getElementById("images-container")
+
+	submitImage.addEventListener('click', function() {
+		event.preventDefault()
+		createNewImage();
+		updateForm()
+		imagesContainer.innerHTML = allImages.map(image => renderImages(image))
+	})
+
+	function updateForm() {
+		imageUrl.value = ""
+		imageCaption.value = ""
+		$("#new-image").modal('close')
+	}
+
+	function createNewImage() {
+		let newUrl = imageUrl.value
+		let newCaption = imageCaption.value
+		let newImage = {}
+		newImage["url"] = newUrl
+		newImage["caption"] = newCaption
+		allImages.push(newImage)
+	}
+
+	function renderImages(image) {
+		return `
+		<div id="wrapper" style="width:100%; text-align:center">
+			<img src="${image.url}"/>
+			<p>${image.caption}</p>
+		</div>`
+	}
+
 })
